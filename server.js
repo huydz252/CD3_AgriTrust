5321964d17efbef9972ccd85b31d7d2c90e39711
Trần Quang Huy <252quanghuy@gmail.com>
2026-04-08 04:09:03 +0700

    dont QR

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static('src/public'));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './src/views'));
 
--- src/controller/productController.js
+++ src/controller/productController.js
@@ -1,4 +1,5 @@
 const getContract = require('../config/blockchain');
+const QRCode = require('qrcode')
 
 const productController = {
     getProjectName: async (req, res) => {
@@ -59,7 +60,6 @@ const productController = {
             const id  = req.params.id;
             const contract = await getContract();
             const history = await contract.getHistory(id);
-
             const productDetail = await contract.getProductDetail(id)
             
             const formattedTimeline = history.map(h => ({
@@ -69,13 +69,20 @@ const productController = {
             // Format ngày tháng theo giờ Việt Nam
             timestamp: new Date(Number(h.timestamp) * 1000).toLocaleString('vi-VN'), 
             performer: h.performer
+            
         }));
 
-        
-            
+        //qr
+        const protocol = req.protocol;
+        const myIP = "192.168.1.16";
+        const qrUrl = `http://${myIP}:3000/api/history/${id}`;
+        // Tạo mã QR dạng chuỗi ảnh (Data URL)
+        const qrImage = await QRCode.toDataURL(qrUrl);
+
         res.render('productHistory', { 
             product: productDetail, 
-            list: formattedTimeline 
+            list: formattedTimeline,
+            qrCode: qrImage
         });
         } catch (error) {
             console.error("Lỗi Controller:", error);
